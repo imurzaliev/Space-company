0f6ac4de2a50953b4d6cd2b03f1a750073c314b2
imurzaliev <imurzalievi@gmial.com>
2022-05-22 00:14:24 +0300

    Merge css files to fix css property issues, add changes to nav bar for middle sized screens

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const toggleButton = document.querySelector('[data-toggle-button]');
 const menu = document.querySelector('[data-menu]');
 
 toggleButton.addEventListener('click', () => {
-  menu.classList.toggle('active');
+  menu.classList.toggle('open');
   toggleButton.classList.toggle('humburger-x');
 });
